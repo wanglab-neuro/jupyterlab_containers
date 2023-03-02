846e37ef9fa28ad6eab655631e9a8885c774e5f6
Vincent <prevosto@mit.edu>
2023-03-01 19:03:41 -0500

    debugging javascript error

--- jlab_caiman/build.sh
+++ jlab_caiman/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -t wanglabneuro/jlab_caiman:latest -t wanglabneuro/jlab_caiman:0.2 -f dockerfiles/Dockerfile context
+# docker build -t wanglabneuro/jlab_caiman:latest -t wanglabneuro/jlab_caiman:0.2 -f dockerfiles/Dockerfile context
+docker build -t wanglabneuro/jlab_caiman:debug -f dockerfiles/Dockerfile_debug context
