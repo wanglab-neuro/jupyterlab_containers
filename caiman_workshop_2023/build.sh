f8a18e262772f0a87082315c7c8752cf74f874c5
Vincent <prevosto@mit.edu>
2023-03-02 18:46:09 -0500

    ~ debugged ipywidgets error

--- caiman_workshop_2023/build.sh
+++ caiman_workshop_2023/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t wanglabneuro/jlab_caiman-workshop23:latest -t wanglabneuro/jlab_caiman-workshop23:0.1 -f Dockerfile .
+docker build -t wanglabneuro/jlab_caiman-workshop23:latest -t wanglabneuro/jlab_caiman-workshop23:0.2 -f Dockerfile .
--- jlab_caiman/build.sh
+++ jlab_caiman/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# docker build -t wanglabneuro/jlab_caiman:latest -t wanglabneuro/jlab_caiman:0.2 -f dockerfiles/Dockerfile context
-docker build -t wanglabneuro/jlab_caiman:debug -f dockerfiles/Dockerfile_debug context
+docker build -t wanglabneuro/jlab_caiman:latest -t wanglabneuro/jlab_caiman:0.2 -f dockerfiles/Dockerfile context
+# docker build -t wanglabneuro/jlab_caiman:debug -f dockerfiles/Dockerfile_debug context
