de6f9cab4afb7c63ab9b77adec2d8aafc23b943c
Vincent <prevosto@mit.edu>
2024-04-01 15:45:11 -0400

    updates to jmatlab

--- jlab_matlab/build.sh
+++ jlab_matlab/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t wanglabneuro/jlab_matlab:latest -t wanglabneuro/jlab_matlab:2021b -f dockerfiles/Dockerfile context
+# docker build -t wanglabneuro/jlab_matlab:latest -t wanglabneuro/jlab_matlab:2021b -f dockerfiles/Dockerfile context
+
+docker build -t wanglabneuro/jlab_matlab:latest -f dockerfiles/Dockerfile_test context
--- jlab_multi/build.sh
+++ jlab_multi/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t wanglabneuro/jlab_multi:latest -t wanglabneuro/jlab_multi:0.1 -f dockerfiles/Dockerfile context
+docker build -t wanglabneuro/jlab_multi:latest -t wanglabneuro/jlab_multi:0.2 -f dockerfiles/Dockerfile context
