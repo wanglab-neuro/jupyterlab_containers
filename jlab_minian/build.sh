545a66ecc7f062e38f5293b7e04f2785e5870949
Vincent <prevosto@mit.edu>
2024-04-12 17:20:43 -0400

    fixes Panel issue

--- jlab_minian/build.sh
+++ jlab_minian/build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker build -t wanglabneuro/jlab_minian:latest -t wanglabneuro/jlab_minian:0.0.2 -f dockerfiles/Dockerfile context
+docker build -t wanglabneuro/jlab_minian:latest -t wanglabneuro/jlab_minian:0.0.3 -f dockerfiles/Dockerfile context
+
+# Versions
+# 0.0.3: fixes Panel issue
+# 0.0.2: Initial working version - with Panel issue (https://github.com/denisecailab/minian/issues/266)
