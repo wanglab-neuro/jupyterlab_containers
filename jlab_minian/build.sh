e4db3fefa209ce9c9bdf736caa9a50ea9efb9f06
Vincent <prevosto@mit.edu>
2024-04-15 17:00:42 -0400

    v0.0.4

--- jlab_minian/build.sh
+++ jlab_minian/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker build -t wanglabneuro/jlab_minian:latest -t wanglabneuro/jlab_minian:0.0.3 -f dockerfiles/Dockerfile context
+docker build -t wanglabneuro/jlab_minian:latest -t wanglabneuro/jlab_minian:0.0.4 -f dockerfiles/Dockerfile context
 
 # Versions
+# 0.0.4: fixes pyviz_comms issue (https://github.com/denisecailab/minian/issues/263)
 # 0.0.3: fixes Panel issue
 # 0.0.2: Initial working version - with Panel issue (https://github.com/denisecailab/minian/issues/266)
